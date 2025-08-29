475308e5ad04568447834ebe72fe5a95ded8cb41
abhishekbapna51 <abhishekbapna51@gmail.com>
2025-08-29 11:16:02 +0530

    chore: add GET helpers for / and /bfhl

--- src/routes/bfhl.routes.js
+++ src/routes/bfhl.routes.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
 const controller = require('../controllers/bfhl.controller');
 
+router.get('/', (_req, res) => {
+  res.status(200).json({ message: 'Use POST /bfhl with body: { "data": ["a","1","2","$"] }' });
+});
+
 router.post('/', controller.postBfhl);
 
 module.exports = router;
